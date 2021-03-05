e20e710758b87aab4e4c0426c57eaf4036ce1593
Louis Leslie <louis.leslie@gmail.com>
2021-03-05 15:50:40 +0000

    Removed the edit csv_file button on the index page. added routes to CSV to allow us to add it if we want

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :csv_files, except: [:edit, :update] do
+  resources :csv_files do
     resources :queries, except: :delete
   end
   resources :queries, only: :delete do
