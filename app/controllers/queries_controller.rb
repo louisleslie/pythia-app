dd60fd5021f804b39c6c75e2210c1850f1d87e48
Louis Leslie <louis.leslie@gmail.com>
2021-03-10 14:26:49 +0000

    file and query show pages now pass the datatypes of each selected order column to the front end as a json file with the id query-json-column-data-types. commented out geocoding on the order model to speed up csv loading times as geocoder not currently needed

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -141,9 +141,19 @@ class QueriesController < ApplicationController
   end
 
   def get_order_datatypes
-    csv_file_order = ["Name",	"Email",	"Financial Status",	"Paid at",	"Fulfillment Status",	"Fulfilled at",	"Accepts Marketing",	"Currency",	"Subtotal",	"Shipping",	"Taxes",	"Total",	"Discount Code",	"Discount Amount",	"Shipping Method",	"Created at",	"Lineitem quantity",	"Lineitem name",	"Lineitem price",	"Lineitem compare at price",	"Lineitem sku",	"Lineitem requires shipping",	"Lineitem taxable",	"Lineitem fulfillment status",	"Billing Name",	"Billing Street",	"Billing Address1",	"Billing Address2",	"Billing Company",	"Billing City",	"Billing Zip",	"Billing Province",	"Billing Country",	"Billing Phone",	"Shipping Name",	"Shipping Street",	"Shipping Address1",	"Shipping Address2",	"Shipping Company",	"Shipping City",	"Shipping Zip",	"Shipping Province",	"Shipping Country",	"Shipping Phone",	"Notes",	"Note Attributes",	"Cancelled at",	"Payment Method",	"Payment Reference",	"Refunded Amount",	"Vendor",	"Id",	"Tags",	"Risk Level",	"Source",	"Lineitem discount",	"Tax 1 Name",	"Tax 1 Value",	"Tax 2 Name",	"Tax 2 Value",	"Tax 3 Name",	"Tax 3 Value",	"Tax 4 Name",	"Tax 4 Value",	"Tax 5 Name",	"Tax 5 Value",	"Phone",	"Receipt Number",	"Duties",	"Billing Province Name",	"Shipping Province Name"]
     @order_data_types = {}
     Order.columns_hash.map { |k, v| @order_data_types[k] = "#{k}-#{v.sql_type_metadata.type}" }
-    @order_data_types
+    if params[:id]
+      query_fields = JSON.parse(Query.find(params[:id]).fields).flatten
+      @selected_columns = {}
+      Order.columns_hash.map do |k, v|
+        if query_fields.include?(k)
+          @selected_columns[k] = v.sql_type_metadata.type
+        end
+      end
+    else
+      @selected_columns = {}
+      Order.columns_hash.map { |k, v| @selected_columns[k] = v.sql_type_metadata.type }
+    end
   end
 end
