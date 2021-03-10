5248a32286146009e072d31152881e4bd8e3aae5
jake howlett <jake.howlett93@gmail.com>
2021-03-10 13:20:16 +0000

    remove extra line

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,5 @@ class Order < ApplicationRecord
   belongs_to :csv_file
   geocoded_by :full_billing_address
   after_validation :geocode, if: :will_save_change_to_full_billing_address?
-
   scope :with_scope, -> (array) {where(id: array.map(&:id))}
 end
